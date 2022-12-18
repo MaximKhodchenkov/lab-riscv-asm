0b654e6bfc87b4483dace7e2cdc00e0ebb0ffe4f
Maxim Khodchenkov <pridymalnikc048@gmail.com>
2022-12-19 00:19:58 +0300

    add all files

--- find_column_with_max_sum.c
+++ find_column_with_max_sum.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-#define N 3
-#define M 3
+#define N 6
+#define M 6
 
-void process(int n, int m, int matrix [M][N], int *result)
+void process(int n, int m, int matrix [N][M], int *result)
 {
     int current_sum = 0;
-    int largest_sum = 0;
-
+    int largest_sum = -2147483648;
+  
     (*result)  = 0;
-
     for (int i = 0; i < m; ++i) {
         current_sum = 0;
         for (int j = 0; j < n; ++j) {
-            current_sum += matrix[i][j];
+            current_sum += matrix[j][i];
         }
-        if (current_sum > largest_sum) {
+        
+        if (current_sum > largest_sum) {            
             largest_sum = current_sum;
             (*result)  = i;
         }
-        
-//      else if (i == 0) {
-//          largest_sum = current_sum;
-//          (*result) = 0;
-//      }            
+  
+//        if (i == 0) {
+//            largest_sum = current_sum;
+//            (*result) = 0;
+//        }            
         
     }
 
@@ -31,19 +31,21 @@ void process(int n, int m, int matrix [M][N], int *result)
 
 int main(int argc, char** argv)
 {
-    int matrix[M][N];
+    int matrix[M][N] = {
+    {-2, -94, -13, -5,    0, -21},
+    {-1,  -4, -13, -5,    0, -21},
+    {-5,  -2, -10, -5,    0, -21},
+    {-1, -46,  -1, -5,    0, -21},
+    {-5, -23,  -7, -5,    0, -21},
+    {-2, -74,  13, -5, -100,-200}};
+
     int result;
-    
-    for (int i = 0; i < M; ++i) {
-        for (int j = 0; j < N; ++j) {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
+
 
     process(N, M, matrix, &result);
 
 
-        printf("%d ", result);
+        printf("%d column with maximum sum of elements\n", result);
 
 
     return 0;
